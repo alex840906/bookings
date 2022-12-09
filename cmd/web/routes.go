8cf206ebf97b67052c67030b08675b0e1b80c19c
Po Hsun <pohsun@PodeMacBook-Air.local>
2022-12-10 01:21:27 +0800

    First commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/alex840906/bookings/pkg/config"
+	"github.com/alex840906/bookings/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	//reset web three times, will show console three times
+	//mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
